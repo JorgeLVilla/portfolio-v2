97eda20b20a919ba42926a7711f1ed4e690f5a40
Jorge Villalobos <79444943+JorgeLVilla@users.noreply.github.com>
2023-03-10 00:00:07 -0800

    feat(navbar): init pages and navbar

---
+++ src/assets/data/NavData.tsx
@@ -0,0 +1,18 @@
+export const NAV_LINKS = [
+  {
+    path: "Home",
+    display: "home",
+  },
+  {
+    path: "About",
+    display: "about",
+  },
+  {
+    path: "Work",
+    display: "work",
+  },
+  {
+    path: "Contact",
+    display: "contact",
+  },
+];
---
+++ src/pages/Navbar.tsx
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { RiMenu5Fill } from "react-icons/ri";
+import { NAV_LINKS } from "@/assets/data/NavData";
+import { INFO } from "@/assets/data/info";
+
+const Navbar = () => {
+  const [navbarOpen, setNavbarOpen] = useState(false);
+
+  const handleNavbar = () => {
+    setNavbarOpen(!navbarOpen);
+  };
+
+  return (
+    <nav className="pt-20 pb-5">
+      {/*------Desktop Navbar Section----------*/}
+      <section className="flex justify-around">
+        <div
+          className="text-2xl
+          "
+        >
+          <Link href={"/"}>Portfolio 2023</Link>
+        </div>
+        <div className="text-4xl sm:hidden" onClick={handleNavbar}>
+          <RiMenu5Fill />
+        </div>
+        <ul className="hidden sm:flex text-xl">
+          {NAV_LINKS.map((item, index) => (
+            <li className="px-5" key={index}>
+              <Link href={`${item.path}`}>{item.display}</Link>
+            </li>
+          ))}
+        </ul>
+      </section>
+      <div className="mt-16 h-[1.5px] w-11/12 bg-black mx-auto"></div>
+      {/*------Open Mobile Navbar----------*/}
+      <section
+        className={`${
+          navbarOpen
+            ? "hidden"
+            : "block py-10 pr-10 absolute inset-0 h-screen bg-black text-off-white"
+        }`}
+      >
+        <div className="pt-10 text-4xl flex justify-end" onClick={handleNavbar}>
+          <RiMenu5Fill />
+        </div>
+        <ul className="pt-24">
+          {NAV_LINKS.map((item, index) => (
+            <li className="text-5xl py-3 px-4 uppercase" key={index}>
+              <Link href={`${item.path}`}>{item.display}</Link>
+            </li>
+          ))}
+        </ul>
+        <ul className="py-10 px-4">
+          <li className="py-3">{INFO.email}</li>
+          <li className="py-2 text-2xl">{INFO.job}</li>
+          <li className="py-2 text-lg">{INFO.location}</li>
+        </ul>
+      </section>
+    </nav>
+  );
+};
+
+export default Navbar;
